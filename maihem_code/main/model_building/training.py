cd8c6bdc3ab4068ddd887e72881f28e55d506dde
Pierre-Lib <liboureau.pierre@gmail.com>
2025-01-22 13:51:54 +0100

    new_package

--- maihem_code/main/model_building/training.py
+++ maihem_code/main/model_building/training.py
@@ -37,7 +37,7 @@ def train_model(dataset_path,
         save_name = f'my_new_{model_architecture}_model'
 
     #Initialise YOLO model
-    model = YOLO(f'{model_architecture}.yaml')
+    model = YOLO(model_architecture)
 
     #Train the model
     model.train(data = dataset_path,
@@ -52,9 +52,9 @@ def train_model(dataset_path,
 
     print(f"Training completed. Model saved to {save_name}.pt")
 
-#d = {'epochs': 50, 'batch_size': 16, 'img_size': 640, 'device': 'cpu', 'seed': 12}
-#train_model(dataset_path="coco8-seg.yaml", hyperparameters= d, save_path = '/Users/pierreliboureau/Downloads/mltest', save_name = "?", model_architecture = "yolo11n-seg")
+d = {'epochs': 50, 'batch_size': 16, 'img_size': 640, 'device': 'cpu', 'seed': 0}
+train_model(dataset_path="coco128-seg.yaml", hyperparameters= d, save_path = '/Users/pierreliboureau/Downloads/mltest', save_name = "?", model_architecture = "yolo11n-seg")
 
-model = YOLO('yolo11n-seg')
-
-model.train(data = 'coco8-seg.yaml', seed = 0)
+# model = YOLO('yolo11n-seg')
+#
+# model.train(data = 'coco8-seg.yaml', seed = 0)
