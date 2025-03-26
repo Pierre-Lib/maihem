f6716e2ddedb10131ce41ff4937fcfc696ef66de
Pierre-Lib <liboureau.pierre@gmail.com>
2025-03-26 19:25:05 +0100

    Finishing touches

--- maihem_code/core/main.py
+++ maihem_code/core/main.py
@@ -12,14 +12,16 @@ def maihem_main(input_file=None):
     input_file : str
         The input file to execute
     """
-    if not input_file:
-        raise TypeError("No input file provided; ending run")
-    run = Runner(input_file)
-    run.read_instructions()
-    run.check_instructions()
-    run.plan_training()
-    run.plan_usage()
-    training_results = run.execute_training()
-    usage_results = run.execute_usage()
-    print(training_results)
-    print(usage_results)
+    try:
+        run = Runner(input_file)
+    except:
+        print("No input file provided; ending run")
+    else:
+        run.read_instructions()
+        run.check_instructions()
+        run.plan_training()
+        run.plan_usage()
+        training_results = run.execute_training()
+        usage_results = run.execute_usage()
+        print(training_results)
+        print(usage_results)
